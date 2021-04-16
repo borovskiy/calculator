74f67a94827d3c2f58a1002ad9c57c549dec3ee7
Александр <russkiy1111@yandex.ru>
2021-04-16 17:06:27 +0300

    2v.calc

---
+++ calculator_fields_settings.py
@@ -0,0 +1,81 @@
+
+dict_button = {
+    "Button_0":
+        ['0', (5, 340, 111, 51)],
+    "Button_1":
+        ['1', (5, 280, 51, 51)],
+    "Button_2":
+        ['2', (60, 280, 51, 51)],
+    "Button_3":
+        ['3', (120, 280, 51, 51)],
+    "Button_4":
+        ['4', (5, 220, 51, 51)],
+    "Button_5":
+        ['5', (60, 220, 51, 51)],
+    "Button_6":
+        ['6', (120, 220, 51, 51)],
+    "Button_7":
+        ['7', (5, 160, 51, 51)],
+    "Button_8":
+        ['8', (60, 160, 51, 51)],
+    "Button_9":
+        ['9', (120, 160, 51, 51)],
+    "Button_division":
+        ['/', (240, 280, 51, 51)],
+    "Button_substraction":
+        ['-', (180, 220, 51, 51)],
+    "Button_addition":
+        ['+', (240, 220, 51, 51)],
+    "Button_pos_neg":
+        ['+/-', (180, 100, 111, 51)],
+    "Button_point":
+        ['.', (120, 340, 51, 51)],
+    "Button_multiplication":
+        ['*', (180, 280, 51, 51)],
+    "Button_total":
+        ['=', (180, 340, 111, 51)],
+    "Button_sin":
+        ['sin', (180, 160, 51, 51)],
+    "Button_cos_2":
+        ['cos', (240, 160, 51, 51)],
+    "Button_del_one_position":
+        ['<-', (5, 100, 51, 51)],
+    "Button_clean_3":
+        ['C', (60, 100, 51, 51)],
+    "Button_sqr":
+        ['sq', (120, 100, 51, 51)],
+}
+text_field_customization_dictionary = {
+    'text_main_lines':
+        {
+            'geometry': (0, 40, 291, 51),
+            'setPointSize': 20,
+            'setBold': False,
+            'setWeight': 50,
+            'setStyleSheet': "background-color: rgb(255, 255, 255);\n""color: rgb(170, 0, 0);",
+            'setText': '0',
+            'setObjectName': "text_enter",
+        },
+
+    'text_additional_line':
+        {
+            'geometry': (0, 10, 261, 21),
+            'setPointSize': 8,
+            'setBold': False,
+            'setWeight': 50,
+            'setStyleSheet': "background-color: rgb(255, 255, 255);\n""color: rgb(170, 0, 0);",
+            'setText': '',
+            'setObjectName': "text_enter_2",
+        },
+
+    'text_operation':
+        {
+            'geometry': (270, 10, 21, 21),
+            'setPointSize': 8,
+            'setBold': False,
+            'setWeight': 50,
+            'setStyleSheet': "background-color: rgb(255, 255, 255);\n""color: rgb(170, 0, 0);",
+            'setText': '',
+            'setObjectName': "text_operation",
+        },
+}
